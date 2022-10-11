880d144b6d36b5dfcf6da9bb81b17b0eb6acb5c5
ceciliamedeiros <mceciliamedeiros1@gmail.com>
2022-10-10 22:51:07 -0300

    incluindo o drop table no migration

--- migrations/20221011014326-disciplina.js
+++ migrations/20221011014326-disciplina.js
@@ -35,11 +35,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Disciplinas');  
   }
 };
